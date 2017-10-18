6dd4017cb8dfe29c25511d79f57cd1dfdf0ab3ba
Juggrid Chetwichian <champ_psu@outlook.co.th>
2017-10-19 06:33:29 +0700

    +index and + shop

---
+++ js/app.js
@@ -0,0 +1,70 @@
+function fncSubmit()
+{   
+
+    if($('#URL').val() == ""){
+        $(function() { 
+            $('#notify').click();
+        });
+        return false;
+
+    }
+
+    if($('#ProductName').val() == ""){
+        $(function() { 
+            $('#notify1').click();
+        });
+        return false;
+
+    }
+
+    if(document.form2.Catagories.value=="*"){
+        $(function() { 
+            $('#notify2').click();
+        });
+        return false;
+    } 
+
+    if(document.form2.Grade.value=="none"){
+        $(function() { 
+            $('#notify3').click();
+        });
+        return false;
+    }
+
+    if(document.form2.Price.value==""){
+        $(function() { 
+            $('#notify4').click();
+        });
+        return false;
+    }
+
+    if(document.form2.Detail.value==""){
+        $(function() { 
+            $('#notify5').click();
+        });
+        return false;
+    }
+
+
+    if(document.form2.shipment.value==""){
+        $(function() { 
+            $('#notify6').click();
+        });
+        return false;
+    }
+
+    if(document.form2.Fee.value==""){
+        $(function() { 
+            $('#notify7').click();
+        });
+        return false;
+    }
+
+    else{
+        
+        
+    }
+
+    document.form2.submit();
+
+}
